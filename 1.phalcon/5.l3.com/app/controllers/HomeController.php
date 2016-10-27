9f65b5f3440c80bbc1112c6c64d3de2258781277
Long Liu Lo <info@longliulo.co>
2016-10-27 14:57:43 +0700

    source

---
+++ 1.phalcon/5.l3.com/app/controllers/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace MyApp\Controllers;
+use Phalcon\Mvc\View;
+class HomeController extends ControllerBase
+{
+    public function indexAction() {
+    }
+
+}
+
---
+++ 1.phalcon/5.l3.com/public/define_value.php
@@ -0,0 +1,22 @@
+<?php
+define('DEFAULT_NAME', "WEBSITE");
+
+define('ITEM_PER_PAGE', 10);
+define('ITEM_PROJECT_PAGE', 9);
+define('ITEM_PER_PAGE_USER', 5);
+
+define('GROUP_SYS_ADMIN', 1);
+define('GROUP_ADMIN', 2);
+define('GROUP_USER', 3);
+define('GROUP_USER_ADMIN', 4);
+
+define('DELETED', 1);
+define('NOT_DELETED', 0);
+
+define('STATUS_ACTIVE', "1");
+define('STATUS_DISACTIVE', "0");
+
+
+
+
+
